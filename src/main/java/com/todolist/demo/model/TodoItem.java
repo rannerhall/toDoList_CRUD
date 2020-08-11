a0701e1f0c37be7679f4d2e3e26b5b63a51b6c88
kapten <r.annerhall@hotmail.com>
2020-08-11 18:10:05 +0200

    minor refactoring

--- src/main/java/com/todolist/demo/model/TodoItem.java
+++ src/main/java/com/todolist/demo/model/TodoItem.java
@@ -1,12 +1,16 @@
 package com.todolist.demo.model;
 
-import lombok.Data;
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.Date;
 
 @Data
 @Entity
+@NoArgsConstructor
+@ToString
+@Getter
+@Setter
 public class TodoItem {
 
     @Id
@@ -17,41 +21,5 @@ public class TodoItem {
     private String taskName;
     @Column(name = "createdDate")
     private Date createdDate;
-
-    public TodoItem() {
-    }
-
-    public Long getItemId() {
-        return itemId;
-    }
-
-    public void setItemId(Long itemId) {
-        this.itemId = itemId;
-    }
-
-    public String getTaskName() {
-        return taskName;
-    }
-
-    public void setTaskName(String taskName) {
-        this.taskName = taskName;
-    }
-
-    public Date getCreatedDate() {
-        return createdDate;
-    }
-
-    public void setCreatedDate(Date createdDate) {
-        this.createdDate = createdDate;
-    }
-
-    @Override
-    public String toString() {
-        return "TodoItem{" +
-                "itemId=" + itemId +
-                ", taskName='" + taskName + '\'' +
-                ", createdDate=" + createdDate +
-                '}';
-    }
 }
 
